6769426fe81d7c94452c332b7c742dc739caeedb
willdrysdale <wsd500@york.ac.uk>
2023-10-30 14:47:07 +0000

    calculate data coverage

---
+++ scripts/db_coverage.R
@@ -0,0 +1,23 @@
+library(DBI)
+library(here)
+library(dplyr)
+library(lubridate)
+
+con = dbConnect(duckdb::duckdb(),
+                dbdir = here("data","db.duckdb"), read_only = FALSE)
+
+
+ts = tibble(date = seq(ymd_hm("2000-01-01 00:00"), ymd_hm("2022-01-01 00:00"), "hour"))
+x = nrow(ts)
+
+coverage = tbl(con, "all_data") |>
+  group_by(station_id, name) |>
+  filter(!is.na(value)) |>
+  count() |>
+  mutate(perc = (n/x)*100) |>
+  collect()
+
+dbWriteTable(con, "coverage", coverage, overwrite = T)
+
+dbDisconnect(con, shutdown = T)
+
