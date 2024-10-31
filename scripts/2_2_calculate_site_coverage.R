defa50958e87463d3fbcdc6ee1236adc5310e743
willdrysdale <wsd500@york.ac.uk>
2024-10-31 14:23:46 +0000

    Correct coverage definition
    Note at this time the db will not be updated with these changes, so need to recheck coverage around step 5_4

--- scripts/2_2_calculate_site_coverage.R
+++ scripts/2_2_calculate_site_coverage.R
@@ -8,12 +8,16 @@ con = dbConnect(duckdb::duckdb(),
 
 # still keep the coverage check over 2000-2022 as the toar database doesn't have the extra data,
 # then any more data we get from the eea is a bonus
-ts = tibble(date = seq(ymd_hm("2000-01-01 00:00"), ymd_hm("2022-01-01 00:00"), "hour"))
+startDate = ymd_hm("2000-01-01 00:00")
+endDate = ymd_hm("2022-01-01 00:00")
+
+ts = tibble(date = seq(startDate, endDate, "hour"))
 x = nrow(ts)
 
 coverage = tbl(con, "all_data") |>
   group_by(station_id, name) |>
-  filter(!is.na(value)) |>
+  filter(!is.na(value),
+         between(date, startDate, endDate)) |>
   count() |>
   mutate(perc = (n/x)*100) |>
   collect() |>
