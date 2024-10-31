8aa9eae1faf878d980c7a51c1f2b6f3fa4f6d839
willdrysdale <wsd500@york.ac.uk>
2024-10-31 10:52:59 +0000

    Update 5_x_calculate_r2.R

--- scripts/5_x_calculate_r2.R
+++ scripts/5_x_calculate_r2.R
@@ -5,7 +5,7 @@ library(tidyr)
 library(lubridate)
 
 con = dbConnect(duckdb::duckdb(),
-                dbdir = here(readLines("data_config.txt",n = 1),"data","db.duckdb"), read_only = FALSE)
+                dbdir = here(readLines(here("data_config.txt"),n = 1),"data","db.duckdb"), read_only = FALSE)
 
 
 # join regeression scenarios to piecewise so we can recover piece
@@ -17,61 +17,61 @@ piecewise = tbl(con, "piecewise") |>
     by = c("station_id","name","scenario_idx")
   )
 
-# single qr data
-qr = tbl(con,"qr_stat") |>
-  filter(type == "fit",
-         tau == 0.5) |>
-  pivot_wider(names_from = "stat")
-
 # select relevant columns from anom data
 anom = tbl(con, "monthly_anom") |>
   select(date, station_id, name, anom, x)
 
 # used to recover piecewise grouping
-# just set arbitrarily in the future
+# just set arbitrarily in the future/past
+mindate = ymd("1990-01-01")
 maxdate = ymd("2030-01-01")
 
-# separate piecewise from qr and loess due as scenario_idx only applies to piecewise
+# deal with QRs and Loess spearately due to scenario_idx
+# put anom and QRs (normal and piecewise) in the same data.frame
 anom_piecewise = piecewise |>
   left_join(anom,
             by = c("x", "station_id", "name")) |>
-  mutate(cp2 = ifelse(is.na(cp2), maxdate, cp2), # replace cp2 NAs with max date
-         piece = case_when(date < cp1 ~ "1",
-                           date >= cp1 & date < cp2 ~ "2",
-                           date >= cp2 ~ "3")
+  mutate(
+    cp1 = ifelse(is.na(cp1), mindate, cp1),
+    cp2 = ifelse(is.na(cp2), maxdate, cp2), # replace cp2 NAs with max date
+    piece = case_when(
+      date < cp1 ~ "1", # 1 change point
+      date >= cp1 & date < cp2 ~ "2",
+      date >= cp2 ~ "3")
   ) |>
   select(x, date,station_id, name, scenario_idx,piece, anom, piecewise) |>
-  pivot_longer(c(piecewise), names_to = "reg")
+  pivot_longer(c(piecewise), names_to = "reg") |>
+  filter(!is.na(date))
 
+# calculate the R^2 between the tau=0.5 QRs (normal and piecewise) and the anomaly timeseries
 anom_piecewise_r2 = anom_piecewise |>
   select(-x, -date) |>
   group_by(station_id, name, reg, scenario_idx) |>
   summarise(r2 = cor(anom, value)^2) |>
   ungroup()
 
-anom_qr_loess = anom |>
+# Put anom and loess in the same data.frame
+anom_loess = anom |>
   left_join(
     tbl(con, "loess"),
     by = c("x", "station_id", "name")
   ) |>
-  left_join(qr,
-            by = c("station_id","name")
-  ) |>
-  mutate(qr = (slope*x)+intercept,
-         scenario_idx = 0) |> # calculate qr
-  select(x, date, station_id, name, anom, qr, loess, scenario_idx) |>
-  pivot_longer(c(qr, loess), names_to = "reg")
+  select(x, date, station_id, name, anom, value = loess) |>
+  mutate(reg = "loess")
 
-anom_qr_loess_r2 = anom_qr_loess |>
+# calcaulte r2 between anom and loess
+anom_loess_r2 = anom_loess |>
   select(-x, -date) |>
-  group_by(station_id, name, reg, scenario_idx) |>
+  group_by(station_id, name, reg) |>
   summarise(r2 = cor(anom, value)^2) |>
   ungroup()
 
-regs = union_all(anom_piecewise, anom_qr_loess) |>
+# get anom, QRs (normal and piecewise) and loess in the same data.frame
+regs = union_all(anom_piecewise, anom_loess) |>
   collect()
 
-regs_r2 = union_all(anom_piecewise_r2, anom_qr_loess_r2) |>
+# get r2 from all regressions in the same data.frame
+regs_r2 = union_all(anom_piecewise_r2, anom_loess_r2) |>
   collect()
 
 dbWriteTable(con, "reg_anom",regs, overwrite = T)
