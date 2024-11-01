cb4c895e591752142eb8b7c1cb0f60abc305a0c2
willdrysdale <wsd500@york.ac.uk>
2024-11-01 15:53:15 +0000

    Update name_station+anom for Ox
    reverse order and remove coverage check from name station.
    It now uses the anom table to get its names and stations, so Ox is captured, and the coverage check was done on No2 and O3 before building anom. This saves us updating the coverage table with Ox, but might decide to do this down the line

--- scripts/2_4_calcualate_anom.R
+++ scripts/2_4_calcualate_anom.R
@@ -1,12 +1,13 @@
 library(DBI)
 library(here)
 library(dplyr)
+library(tidyr)
 library(lubridate)
 
 con = dbConnect(duckdb::duckdb(),
                 dbdir = here(readLines(here("data_config.txt"),n = 1),"data","db.duckdb"), read_only = FALSE)
 
-ts = tibble(date = seq(min(ymd_hms("2000-01-01 00:00:00")), max(ymd_hms("2021-12-31 00:00:00")), "day")) %>%
+ts = tibble(date = seq(min(ymd_hms("2000-01-01 00:00:00")), max(ymd_hms("2023-12-31 00:00:00")), "day")) %>%
   mutate(x = row_number())
 
 dat = tbl(con,"all_data") |>
@@ -14,11 +15,18 @@ dat = tbl(con,"all_data") |>
   left_join(tbl(con, "seasonality"), by = c("name", "station_id","m")) |>
   left_join(tbl(con, "coverage"), by = c("name", "station_id")) |>
   filter(coverage_check) |>
-  select(-m, -coverage_check) |>
+  select(-m, -coverage_check, -n, -perc) |>
   mutate(date = floor_date(date, "day")) |>
   group_by(date, station_id, station_type, name) |>
   summarise_all(median, na.rm = T) |>
   mutate(anom = value-season) |>
+
+  pivot_wider(values_from = c("anom","value", "season"), names_sep = "_") |>
+  mutate(anom_ox = anom_o3+anom_no2) |>
+  pivot_longer(-c(date, station_id, station_type, lat, lng), names_to = c("type", "name"), names_sep = "_") |>
+  pivot_wider(names_from = "type") |>
+
+  filter(!is.na(anom)) |>
   collect() |>
   left_join(ts, by = "date")
 
--- scripts/2_5_create_name_station_after_coverage.R
+++ scripts/2_5_create_name_station_after_coverage.R
@@ -5,11 +5,11 @@ library(dplyr)
 con = dbConnect(duckdb::duckdb(),
                 dbdir = here(readLines(here("data_config.txt"),n = 1),"data","db.duckdb"), read_only = FALSE)
 
-name_station = tbl(con,"all_data") |>
+name_station = tbl(con,"anom") |>
   select(name, station_id) |>
   distinct() |>
-  left_join(tbl(con,"coverage"), by = c("name","station_id")) |>
-  filter(coverage_check) |>
+  # left_join(tbl(con,"coverage"), by = c("name","station_id")) |>
+  # filter(coverage_check) |>
   select(name, station_id) |>
   collect()
 
