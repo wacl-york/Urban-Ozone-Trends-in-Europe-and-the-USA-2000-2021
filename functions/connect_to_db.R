acc238028b1fa9dd7ac212a7d96aa0615eb9fa80
willdrysdale <wsd500@york.ac.uk>
2024-11-11 13:46:19 +0000

    Add DBI:: to connect_to_db()

--- functions/connect_to_db.R
+++ functions/connect_to_db.R
@@ -1,7 +1,7 @@
 connect_to_db = function(read_only = TRUE){
-  con = dbConnect(duckdb::duckdb(),
-                  dbdir = here::here(readLines(here::here("data_config.txt"),n = 1),"data","db.duckdb"),
-                  read_only = read_only)
+  con = DBI::dbConnect(duckdb::duckdb(),
+                        dbdir = here::here(readLines(here::here("data_config.txt"),n = 1),"data","db.duckdb"),
+                        read_only = read_only)
 
   return(con)
 }
