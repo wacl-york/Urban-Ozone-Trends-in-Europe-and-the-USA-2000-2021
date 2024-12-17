840f6937e044938d5867b4b959d43314f3289d82
bsn502 <bsn502@york.ac.uk>
2024-12-17 15:13:38 +0000

    create RDS for qr_piece (changepoint plots coloured by year)

--- analysis/map_top_changing_slopes_2.R
+++ analysis/map_top_changing_slopes_2.R
@@ -8,6 +8,8 @@ library(rnaturalearth)
 library(rnaturalearthhires)
 library(patchwork)
 library(ggpubr)
+library(duckdb)
+library(DBI)
 
 source(here::here('functions','connect_to_db.R'))
 
@@ -17,13 +19,19 @@ qr_piece$country[qr_piece$country != "United States of America"] = "Europe"
 qr_piece$A_to_B = abs(qr_piece$A_to_B)
 qr_piece$B_to_C = abs(qr_piece$B_to_C)
 
+con = connect_to_db()
+
+blacklist = tbl(con, "remove_sites") |>
+  collect() |>
+  rename(name = "spc")
 
 qr_piece_long <- qr_piece |>
   pivot_longer(cols = c("A_to_B", "B_to_C"), names_to = "transformation", values_to = "slope_mag") |>
   unnest_wider(startYears, names_sep = "_") |>
   mutate(flip_dir = ifelse(transformation == "A_to_B", A_to_B_dir_flip, B_to_C_dir_flip),
          year = ifelse(transformation == "A_to_B", startYears_2, startYears_3)) |>
-  select(-c(startYears_1, startYears_2, startYears_3, A_to_B_year, B_to_C_year, A_to_B_dir_flip, B_to_C_dir_flip))
+  select(-c(startYears_1, startYears_2, startYears_3, A_to_B_year, B_to_C_year, A_to_B_dir_flip, B_to_C_dir_flip)) |>
+  anti_join(blacklist, by = c("station_id", "name"))
 
 
 col = c(
@@ -35,14 +43,6 @@ col = c(
   "#5F903D",
   "#C0D9A1"
 )
-
-# qr_piece |>
-#   filter(name %in% c("o3", "no2")) |>
-#   ggplot(aes(x = B_to_C))+
-#   geom_density_ridges(aes(fill = factor(tau), y = factor(tau))) +
-#   facet_grid(~name~country, scales = "free_y") +
-#   scale_fill_manual(values = col) +
-#   theme_minimal()
 
 mycrs = 4087 #8857
 
@@ -252,6 +252,20 @@ no2_slope_max = max(qr_piece_rm_nas$slope_mag[qr_piece_rm_nas$name == "no2"])
 limit_year_min = 2000
 limit_year_max = 2023
 
+states = rnaturalearth::ne_states(returnclass = "sf") |>
+  filter(admin %in% c("United States of America","Puerto Rico")) |>
+  select(name)
+
+usMeta = tbl(con, "combinedMeta") |>
+  filter(country == "United States of America") |>
+  collect() |>
+  st_as_sf(coords = c("longitude", "latitude"), crs = st_crs("WGS84")) |>
+  st_join(states, join = st_nearest_feature) |>
+  select(station_id, state = name) |>
+  st_drop_geometry()
+
+dbDisconnect(con)
+
 png("~/TOAR/TOAR_paper/plots/US_o3_tau_0.5_changepoint_year_mag_map.png",width = 1080*3.5, height = 1080*2.5, res = 300)
 ggplot() +
   geom_sf(data = world, fill = "white")+
@@ -329,3 +343,19 @@ ggplot() +
   ggtitle(expression("NO"[2]))
 dev.off()
 
+datScrape = qr_piece_long_sf |>
+  st_drop_geometry() |>
+  filter(transformation == "B_to_C",
+         country != "United States of America",
+         tau == 0.5,
+         name == "no2") |>
+  left_join(usMeta, by = "station_id") |>
+filter(
+  flip_dir == "positive_to_negative") |>
+  #flip_dir == "negative_to_positive") |>
+  #flip_dir %in% c("positive_to_negative", "negative_to_positive")) |>
+  distinct()
+
+
+ggplot(data = datScrape, aes(x = year))+
+  geom_histogram(stat = "count", binwidth = 1)
