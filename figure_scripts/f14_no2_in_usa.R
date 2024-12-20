10205551646dc92f5ff7c4b83fa7295bd503ead8
willdrysdale <will.drysdale@york.ac.uk>
2024-12-20 15:15:32 +0000

    update f14 (including rename of script)

--- figure_scripts/f14_no2_in_usa.R
+++ figure_scripts/f14_no2_in_usa.R
@@ -3,8 +3,10 @@ library(dplyr)
 library(tidyr)
 library(ggtext)
 library(ggplot2)
+library(lubridate)
 
 source(here::here('functions','connect_to_db.R'))
+source(here::here('functions','pad_time_series.R'))
 
 con = connect_to_db()
 
@@ -74,7 +76,7 @@ inc_no2 = slopes_year |>
               names_from = spc) |>
   filter(pv_no2 < 0.33,
          fit_no2 > 0,
-         !is.na(fit_no2)) # |> pull(year) |> max()
+         !is.na(fit_no2)) |> # pull(year) |> max()
   select(-year) |>
   distinct()
 
@@ -97,7 +99,72 @@ for(stn in inc_no2$station_id){
 
 dat = bind_rows(datList)
 
+
+# O3 and Ox that share CP2 with NO2 ---------------------------------------
+no2_cps = slopes_year |>
+  filter(station_id %in% inc_no2$station_id[!is.na(inc_no2$fit_o3)], # only get stations that measure o3 aswell
+         spc == "no2") |>
+  select(spc, station_id, scenario_idx) |>
+  distinct() |>
+  left_join(tbl(con, "regression_scenarios") |>
+              collect(),
+            c("spc" = "name","station_id", "scenario_idx")) |>
+  select(station_id, cp2)
+
+
+
+new_scens = tbl(con, "aic") |>
+  left_join(
+    tbl(con, "regression_scenarios"),
+    by = c("name", "station_id", "scenario_idx")
+  ) |>
+  filter(station_id %in% !!no2_cps$station_id,
+         name %in% c("o3", "ox"),
+         !is.na(cp2)) |>
+  collect() |>
+  inner_join(no2_cps, by = c("station_id", "cp2")) |>
+  group_by(name, station_id) |>
+  filter(aic == max(aic))
+
+
+datListCpAsNo2 = list()
+cpAsNo2StatList = list()
+
+for(i in 1:nrow(new_scens)){
+
+  stn = new_scens$station_id[i]
+  scen_idx = new_scens$scenario_idx[i]
+  nm = new_scens$name[i]
+
+
+  datListCpAsNo2[[i]] = tbl(con,"reg_anom") |>
+    filter(station_id == !!stn,
+           scenario_idx == !!scen_idx,
+           name == !!nm) |>
+    collect() |>
+    pivot_wider(names_from = "reg") |>
+    arrange(date)
+
+  cpAsNo2StatList[[i]] = tbl(con, "qr_regressions") |>
+    filter(station_id == !!stn,
+           scenario_idx == !!scen_idx,
+           name == !!nm,
+           tau == 0.5,
+           stat == "slope") |>
+    collect()
+
+}
+
+datCpAsNo2 = bind_rows(datListCpAsNo2)
+
+
+# Plotting ----------------------------------------------------------------
+
+
 plotDat = dat |>
+  filter(name == "no2") |>
+  bind_rows(
+    datCpAsNo2) |>
   select(date, station_id, name, anom, pqr_2, piece) |>
   mutate(date = lubridate::floor_date(date, "1 month")) |>
   group_by(date, station_id, name, piece) |>
@@ -107,7 +174,13 @@ plotDat = dat |>
                           name == "ox" ~ "O<sub>x</sub>",
                           name == "no2" ~ "NO<sub>2</sub>") |>
            factor(levels = c("O<sub>3</sub>","NO<sub>2</sub>","O<sub>x</sub>"))
-  )
+  ) |>
+  nest_by(name, station_id) |>
+  mutate(data = data |>
+           pad_time_series("1 month") |>
+           list()
+           ) |>
+  unnest(data)
 
 
 g1 = plotDat |>
@@ -128,3 +201,29 @@ g1 = plotDat |>
 pdf("figures/f14_no2_in_usa.pdf", width = 11.7, height = 8.3)
 print(g1)
 dev.off()
+
+
+
+# For numbers -------------------------------------------------------------
+
+no2_before_increase = slopes_year |>
+  filter(
+    country == "United States of America",
+    seg %in% 11:14,
+    tau == 0.5,
+    year == 2014
+  ) |>
+  filter(station_id %in% inc_no2$station_id) |>
+  select(-scenario_idx, -endYear, -aic, -r2, -se) |>
+  pivot_wider(values_from = c(fit, pv),
+              names_from = spc) |>
+  select(-year) |>
+  distinct()
+
+cpAsNo2Stat = bind_rows(cpAsNo2StatList) |>
+  pivot_wider(names_from = type) |>
+  filter(startYear > 2015) |>
+  select(-startYear, -endYear, -se, -scenario_idx) |>
+  pivot_wider(values_from = c(fit, pv),
+              names_from = name) |>
+  mutate(across(contains("fit"), \(x) x*365))
