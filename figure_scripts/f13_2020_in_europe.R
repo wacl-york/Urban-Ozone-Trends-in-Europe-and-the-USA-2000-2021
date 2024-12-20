2863796e016be8aece3f9917fedcb8b65f67a022
willdrysdale <will.drysdale@york.ac.uk>
2024-12-20 12:40:47 +0000

    add f13 2020 in europe

--- figure_scripts/f13_2020_in_europe.R
+++ figure_scripts/f13_2020_in_europe.R
@@ -1,12 +1,12 @@
 library(DBI)
 library(dplyr)
 library(tidyr)
+library(ggtext)
 library(ggplot2)
 
 source(here::here('functions','connect_to_db.R'))
 
 con = connect_to_db()
-
 
 min_aic = tbl(con, "min_aic") |>
   group_by(name, station_id) |>
@@ -62,6 +62,8 @@ slopes_year = left_join(segments, slopes,
               collect(),
             by = "station_id")
 
+# sites where NO2 switched to increasing between 2019 and 2020
+
 inc_no2 = slopes_year |>
   filter(
     year %in% 2019:2020,
@@ -76,18 +78,7 @@ inc_no2 = slopes_year |>
               names_from = year) |>
   filter(fit_2020 > 0 & fit_2019 < 0)
 
-# trend ranges
-inc_no2 |>
-  select(-contains("pv")) |>
-  pivot_longer(contains("fit")) |>
-  mutate(value = value*365/1.96) |>
-  group_by(spc, tau, name) |>
-  summarise(mn = round(min(value),2),
-            mx = round(max(value),2)
-  )
-
 datList = list()
-
 for(stn in inc_no2$station_id){
 
   datList[[stn]] = tbl(con,"reg_anom") |>
@@ -104,71 +95,8 @@ for(stn in inc_no2$station_id){
 
 }
 
-inc_no2_pre2020 = slopes_year |>
-  filter(
-    year %in% 2019:2020,
-    country != "United States of America",
-    seg %in% 11:14,
-    #pv < 0.05,
-    spc == "no2",
-    tau == 0.5
-  ) |>
-  select(station_id, spc, tau, fit, year, pv) |>
-  pivot_wider(values_from = c(fit,pv),
-              names_from = year) |>
-  filter(fit_2020 > 0 & fit_2019 >= 0)
-
-datListpre2020 = list()
-
-for(stn in inc_no2_pre2020$station_id){
-
-  datListpre2020[[stn]] = tbl(con,"reg_anom") |>
-    filter(station_id == !!stn) |>
-    left_join(min_aic, c("scenario_idx", "name", "station_id", "reg")) |>
-    collect() |>
-    mutate(scenario_idx = ifelse(reg == "loess", 0, aic),
-           piece = ifelse(reg == "loess", "1", piece)) |>
-    mutate(aic = ifelse(reg %in% c("qr","loess"), 0, aic)) |>
-    filter(!is.na(aic)) |>
-    select(-aic, -r2, -npiece, -scenario_idx) |>
-    pivot_wider(names_from = "reg") |>
-    arrange(date)
-
-}
-
-
-dat = bind_rows(datList)
-
-datPre2020 = bind_rows(datListpre2020)
-
-# -------------------------------------------------------------------------
-
-dat |>
-  filter(name != "ox") |>
-  mutate(date = lubridate::floor_date(date, "1 month")) |>
-  group_by(date, station_id, name, piece) |>
-  summarise_all(median, na.rm = T) |>
-  ggplot()+
-  geom_line(aes(date, anom/1.96, colour = name))+
-  geom_line(aes(date, pqr_2/1.96, colour = name, group = interaction(name,piece)), linewidth = 2)+
-  facet_wrap(~station_id, scale = "free_y")+
-  theme_minimal()
-
-datPre2020 |>
-  filter(name != "ox") |>
-  mutate(date = lubridate::floor_date(date, "1 month")) |>
-  group_by(date, station_id, name, piece) |>
-  summarise_all(median, na.rm = T) |>
-  ggplot()+
-  geom_line(aes(date, anom/1.96, colour = name))+
-  geom_line(aes(date, pqr_2/1.96, colour = name, group = interaction(name,piece)), linewidth = 2)+
-  facet_wrap(~station_id, scale = "free_y")+
-  theme_minimal()
-
-
-
-# O3 CP? force CP to 2020? ------------------------------------------------
-
+# get best scenario for O3 and Ox where cp2 changes in 2020 also
+# at sites identified by inc_no2
 cp2020 = tbl(con, "aic") |>
   left_join(
     tbl(con, "regression_scenarios"),
@@ -178,7 +106,6 @@ cp2020 = tbl(con, "aic") |>
          year(cp2) == 2020) |>
   group_by(name, station_id) |>
   filter(aic == min(aic, na.rm = T))
-
 
 datListCp2020 = list()
 for(stn in inc_no2$station_id){
@@ -199,6 +126,10 @@ for(stn in inc_no2$station_id){
 
 datCp2020 = bind_rows(datListCp2020)
 
+dat = bind_rows(datList)
+
+# sites where the 2020 slope has p > 0.33 - for removal
+
 scens = datCp2020 |>
   select(station_id, name, scenario_idx) |>
   distinct() |>
@@ -215,21 +146,19 @@ o3_insig_2020 = tbl(con, "qr_regressions") |>
   left_join(
     tbl(con, "regression_scenarios"),
     by = c("name", "station_id", "scenario_idx")
-    ) |>
- # select(-startYear, -endYear, -id) |>
+  ) |>
+  # select(-startYear, -endYear, -id) |>
   pivot_wider(names_from = type) |>
   filter(pv > 0.33,
          name == "o3") |>
   collect()
 
-
+# sites that have both o3 and no2
 o3_no2_sites = datCp2020 |>
   select(name, station_id) |>
   distinct() |>
   pivot_wider(values_from = "name") |> # what a gross use of pivot
   filter(!is.na(ox))
-
-dbDisconnect(con, shutdown = T)
 
 plotDat = dat |>
   bind_rows(
@@ -241,30 +170,35 @@ plotDat = dat |>
   ) |>
   # filter(name != "ox") |>
   select(date, station_id, name, anom, pqr_2, piece) |>
-  mutate(date = lubridate::floor_date(date, "1 month")) |>
+  mutate(date = lubridate::floor_date(date, "1 month"),
+         anom = anom/1.96,
+         pqr_2 = pqr_2/1.96) |> # ug m-3 -> ppb
   group_by(date, station_id, name, piece) |>
   summarise_all(median, na.rm = T) |>
   ungroup() |>
   filter(station_id %in% o3_no2_sites$station_id,
          !station_id %in% o3_insig_2020$station_id,
-         station_id != "gr0031a")
-
-
-plotDat |>
+         station_id != "gr0031a") |>
   filter(name %in% c("no2", "o3_cp2020", "ox_cp2020")) |>
+  mutate(name = case_when(name == "o3_cp2020" ~ "O<sub>3</sub>",
+                           name == "ox_cp2020" ~ "O<sub>x</sub>",
+                           name == "no2" ~ "NO<sub>2</sub>") |>
+           factor(levels = c("O<sub>3</sub>","NO<sub>2</sub>","O<sub>x</sub>"))
+           )
+
+g1 = plotDat |>
   ggplot()+
-  geom_line(aes(date, anom/1.96, colour = name))+
-  geom_line(aes(date, pqr_2/1.96, colour = name, group = interaction(name,piece)), linewidth = 2)+
+  geom_line(aes(date, anom, colour = name))+
+  geom_line(aes(date, pqr_2, group = interaction(name,piece)), linewidth = 1.2, colour = "black")+
+  geom_line(aes(date, pqr_2, colour = name, group = interaction(name,piece)), linewidth = 0.8)+
+  scale_y_continuous(name = "Anomaly Mixing Ratio / ppb")+
+  scale_x_datetime(name = "Date")+
+  scale_colour_manual(values = c("#FF0000", "#F98400", "#00A08A"), name = "")+
   facet_wrap(~station_id, scale = "free_y")+
-  theme_minimal()
+  theme_minimal()+
+  theme(legend.text = element_markdown(),
+        legend.position = "bottom")
 
-
-
-# -------------------------------------------------------------------------
-
-tbl(con, "eeaMeta") |>
-  filter(site %in% !!unique(plotDat$station_id)) |>
-  collect()
-
-
-
+pdf("figures/f13_2020_in_europe.pdf", width = 11.7, height = 8.3)
+print(g1)
+dev.off()
