532f3f3bb8ab42a6968fb10680d3989fa9403865
willdrysdale <will.drysdale@york.ac.uk>
2025-05-01 17:35:38 +0100

    ammend MDA8 calculation to use timezone information

--- scripts/2_6_calculate_MDA8_ozone.R
+++ scripts/2_6_calculate_MDA8_ozone.R
@@ -1,35 +1,62 @@
-library(sf)
-library(DBI)
-library(dplyr)
-library(tidyr)
-library(scico)
-library(ggplot2)
-library(here)
-library(lubridate)
 library(zoo)
-library(plotly)
+library(DBI)
+library(here)
+library(dplyr)
+library(lubridate)
 
 source(here::here('functions','connect_to_db.R'))
+
+calc_mda8 = function(x){
+
+  y = x[!is.na(x)]
+
+  if(length(y) < 6){ # An MDA8 is valid if 6 of 8 hours are present in period
+    return(NA)
+  }
+
+  mean(x, na.rm = T)
+
+}
 
 con = connect_to_db(FALSE)
 
 all_o3_data_collect = tbl(con, "all_data") |>
   filter(name == "o3") |>
-  select(date, station_id, value) |>
+  select(date, station_id, value, name) |>
+  left_join(
+    tbl(con, "combinedMeta") |>
+      select(station_id, timezone),
+    by = "station_id"
+  ) |>
   group_by(station_id) |>
   collect()
 
-MDA8 = all_o3_data_collect |>
-  group_by(station_id) |>
-  mutate(rolling_mean = rollmean(value,
-                                 k = 8,
-                                 fill = NA,
-                                 align = "center"))
-MDA8 = MDA8 |>
-  mutate(name = "o3") |>
-  select(date, station_id, name, value, MDA8 = rolling_mean)
+MDA8_list = list()
 
-#saveRDS(MDA8, "Z:/TOAR_II_BSN/TOAR_paper/scripts/MDA8_ozone.RDS")
+stations = unique(all_o3_data_collect$station_id)
+
+cli::cli_progress_bar(total = length(stations))
+
+for(i in 1:length(stations)){
+  MDA8_list[[i]] = all_o3_data_collect |>
+    filter(station_id == stations[i]) |>
+    mutate(
+      mda8 = rollapply(
+        data = value,
+        width = 8,
+        FUN = calc_mda8,
+        fill = NA,
+        align = "left"),
+      local_date = with_tz(date, timezone)
+    ) |>
+    ungroup() |>
+    mutate(mda8 = ifelse(hour(local_date) %in% 0:6, NA, mda8))
+
+  cli::cli_progress_update()
+}
+
+MDA8 = bind_rows(MDA8_list)
+
+dbWriteTable(con, "mda8_o3", MDA8, overwrite = T)
 
 dbDisconnect(con, shutdown = T)
-
