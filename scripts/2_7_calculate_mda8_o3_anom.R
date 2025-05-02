e827d7e92cea6a765cdb99fa9e8715602145042d
willdrysdale <will.drysdale@york.ac.uk>
2025-05-02 11:44:35 +0100

    create mda8_o3_anom table

---
+++ scripts/2_7_calculate_mda8_o3_anom.R
@@ -0,0 +1,29 @@
+library(DBI)
+library(here)
+
+source(here::here('functions','connect_to_db.R'))
+
+con = connect_to_db(FALSE)
+
+dbExecute(
+  con,
+  '
+  CREATE TABLE mda8_o3_anom AS
+    SELECT q01.*, mda8 - season AS mda8_anom
+    FROM (
+      SELECT seasonality.*, date, "value", timezone, mda8, local_date, "day"
+      FROM seasonality
+      LEFT JOIN (
+        SELECT mda8_o3.*, EXTRACT(MONTH FROM date) AS m
+        FROM mda8_o3
+      ) RHS
+        ON (
+          seasonality."name" = RHS."name" AND
+          seasonality.station_id = RHS.station_id AND
+          seasonality.m = RHS.m
+        )
+    ) q01
+  '
+)
+
+dbDisconnect(con, shutdown = T)
