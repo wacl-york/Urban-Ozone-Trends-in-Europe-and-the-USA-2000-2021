b48cbc3efd911004c45f88d400e4b6337d119f14
bsn502 <beth.nelson@york.ac.uk>
2025-06-18 10:48:03 +0100

    scripts to calculate and write AIC for mda8 and mda8_anom

---
+++ scripts/5_5_0_2_do_aic_mda8_anom.R
@@ -0,0 +1,53 @@
+library(DBI)
+library(here)
+library(dplyr)
+library(tidyr)
+
+do_aic = function(array_id, user){
+
+  dirOut = file.path("/users", user, "scratch", "toar", "aic_mda8_anom")
+  if(!dir.exists(dirOut)){
+    dir.create(dirOut)
+  }
+
+
+  con = dbConnect(duckdb::duckdb(),
+                  dbdir = here(readLines(here("data_config.txt"),n = 1),"data","db.duckdb"), read_only = TRUE)
+
+  on.exit(dbDisconnect(con, shutdown = T))
+
+  nm_stn = tbl(con, "reg_mda8_anom_r2") |>
+    select(station_id, name) |>
+    collect() |>
+    distinct()
+
+  fileOut = file.path(dirOut, paste0("aic_mda8_anom_",nm_stn$station_id[array_id],"_",nm_stn$name[array_id],".csv"))
+
+  dat = tbl(con, "reg_mda8_anom") |>
+    filter(station_id == !!nm_stn$station_id[array_id],
+           name == !!nm_stn$name[array_id],
+           reg != "loess") |>
+    collect() |>
+    nest_by(scenario_idx, reg, name, station_id) |>
+    mutate(npiece = length(unique(data$piece))) |>
+    filter(npiece > 1)
+
+  datAic = dat |>
+    mutate(mod = ifelse(reg == "qr",
+                        list(quantreg::rq(anom ~ x, tau = 0.5, data = data)),
+                        list(quantreg::rq(anom ~ x + piece + piece*x, tau = 0.5, data = data))),
+           aic = AIC(mod)) |>
+    ungroup() |>
+    select(-data, -mod)
+
+  write.csv(datAic, fileOut, row.names = F)
+
+}
+
+user = system("echo $USER", intern = T)
+
+args = commandArgs(trailingOnly = TRUE)
+array_id = as.numeric(args[1])+1
+
+do_aic(array_id, user)
+
