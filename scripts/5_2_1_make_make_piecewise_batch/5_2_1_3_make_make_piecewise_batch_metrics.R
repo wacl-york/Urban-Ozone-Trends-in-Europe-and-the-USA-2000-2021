d6e05d19ea08d8683389fb3a98341b869e80933d
bsn502 <beth.nelson@york.ac.uk>
2025-07-17 17:15:38 +0100

    added in piecewise metric scripts

---
+++ scripts/5_2_1_make_make_piecewise_batch/5_2_1_3_make_make_piecewise_batch_metrics.R
@@ -0,0 +1,45 @@
+library(DBI)
+library(here)
+library(dplyr)
+
+con = dbConnect(duckdb::duckdb(),
+                dbdir = here(readLines(here("data_config.txt"),n = 1),"data","db.duckdb"), read_only = TRUE)
+
+on.exit(dbDisconnect(con, shutdown = T))
+
+metric_station = tbl(con, "mda8_metric_scenarios") |>
+  select(metric, station_id) |>
+  distinct() |>
+  collect() |>
+  arrange(metric, station_id)
+
+user = system("echo $USER", intern = T)
+
+message = c("#!/usr/bin/env bash",
+            "#SBATCH --job-name=toar_make_piecewise_metrics # Job name",
+            "#SBATCH --ntasks=1                      # Number of MPI tasks to request",
+            "#SBATCH --cpus-per-task=8               # Number of CPU cores per MPI task",
+            "#SBATCH --mem=16G                      # Total memory to request",
+            "#SBATCH --time=0-01:00:00               # Time limit (DD-HH:MM:SS)",
+            "#SBATCH --account=chem-cmde-2019        # Project account to use",
+            "#SBATCH --mail-type=END,FAIL            # Mail events (NONE, BEGIN, END, FAIL, ALL)",
+            paste0("#SBATCH --mail-user=",user,"@york.ac.uk   # Where to send mail"),
+            "#SBATCH --output=%x_log/%a/%x-%j.log       # Standard output log",
+            "#SBATCH --error=%x_err/%a/%x-%j.err        # Standard error log",
+            paste0("#SBATCH --array=0-",nrow(metric_station)-1,"          # Array range"),
+            "# Abort if any command fails",
+            "set -e",
+            "",
+            "# purge any existing modules",
+            "module purge",
+            "",
+            "# Load modules",
+            "module load R/4.4.0-gfbf-2023b",
+            "",
+            "# Commands to run",
+            paste0('Rscript --vanilla /mnt/scratch/users/',user,'/TOAR_paper/scripts/5_3_0_create_piecewise/5_3_0_3_create_piecewise_metrics.R $SLURM_ARRAY_TASK_ID')
+)
+
+data_file = file(paste0('/mnt/scratch/users/',user,'/TOAR_paper/sbatch/run_makepiecewise_metrics.sbatch'), open = "wt")
+writeLines(message, con = data_file)
+close(data_file)
