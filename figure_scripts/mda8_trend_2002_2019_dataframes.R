3e222b19aee36ddb9f95b6cd3c005116405c72bf
bsn502 <beth.nelson@york.ac.uk>
2025-08-22 16:23:04 +0100

    looking at start and end periods of mda8 trend data

--- figure_scripts/mda8_trend_2002_2019_dataframes.R
+++ figure_scripts/mda8_trend_2002_2019_dataframes.R
@@ -22,8 +22,8 @@ combined_meta = tbl(con, "combinedMeta") |>
   distinct()
 
 
-tables = dbListTables(con)[str_detect(dbListTables(con), "piecewise_stats_freeTau")]
-tables = "piecewise_stats_freeTau_mda8_anom_all"
+#tables = dbListTables(con)[str_detect(dbListTables(con), "piecewise_stats_freeTau")]
+tables = "piecewise_stats_freeTau_mda8_anom_warm"
 i = 1
 
 p_colours = c(
@@ -95,8 +95,14 @@ mda8_all = tbl(con, tableName) |>
     0.33 >= pv & pv > 0.10 ~ "low_certainty",
     pv > 0.33 ~ "very_low_certainty",
     .default = NA),
-    map_region = ifelse(country == "United States of America", "US", "EU")) |>
-  filter(year %in% c(2002, 2019))
+    map_region = ifelse(country == "United States of America", "US", "EU"))
+# |>
+#  filter(year %in% c(2002, 2019))
+
+combined_meta = tbl(con, "combinedMeta") |>
+  select(station_id, timezone, country, latitude, longitude) |>
+  distinct() |>
+  collect()
 
 dbDisconnect(con)
 
@@ -117,8 +123,76 @@ mda8_uncertainty_count = mda8_all |>
   count() |>
   group_by(map_region, tau, year) |>
   mutate(percentage = n / sum(n)*100) |>
-  pivot_wider(names_from = tau, values_from = c(n, percentage))
+  pivot_wider(names_from = tau, values_from = c(n, percentage)) |>
+  # These filters are used to look at the ranges for the significance bar plots
+  filter(uncertainty == "high_certainty",
+         map_region == "US",
+         direction == "increasing")
 
+ggplot(mda8_uncertainty_count)+
+  geom_point(aes(x = year, y = n_0.95)) +
+  geom_line(aes(x = year, y = n_0.95))
+
+# Trying to explore the MDA8 trends in 95th percentile that are showing high certainty increases#
+
+explore_0.95_inc_high_cert = mda8_all |>
+  filter(tau == 0.95,
+         country == "United States of America",
+         uncertainty %in% c("high_certainty", "very_high_certainty"),
+         fit > 0,
+         year == 2017) |>
+  select(station_id, timezone, fit) |>
+  left_join(combined_meta, by = c("station_id", "timezone")) |>
+  unique()
+
+sites_explore_0.95_inc_high_cert = unique(explore_0.95_inc_high_cert$station_id)
+
+explore_all = mda8_all |>
+  filter(tau == 0.95,
+         country == "United States of America",
+         year == 2017) |>
+  select(station_id, timezone, fit) |>
+  left_join(combined_meta, by = c("station_id", "timezone"))
+
+explore_all_sf = explore_all |>
+  st_as_sf(coords = c(lon = "longitude", lat = "latitude"), crs = 4326) |>
+  mutate(target = ifelse(station_id %in% c(explore_0.95_inc_high_cert$station_id), "yes", "no"))
+
+
+#sites_explore_0.95_inc_high_cert = unique(explore_0.95_inc_high_cert$station_id)
+
+mycrs = 4087 #8857
+
+world = rnaturalearth::ne_coastline(scale = "medium", returnclass = "sf") |>
+  st_transform(mycrs)
+
+limUS = tibble(lng = c(-130,-50), lat = c(25,50)) |>
+  st_as_sf(coords = c("lng", "lat"), crs = st_crs("WGS84")) |>
+  st_transform(mycrs)
+
+limEU = tibble(lng = c(-20,35), lat = c(25,65)) |>
+  st_as_sf(coords = c("lng", "lat"), crs = st_crs("WGS84")) |>
+  st_transform(mycrs)
+
+ggplot() +
+  geom_sf(data = world)+
+  geom_sf(data = explore_all_sf |> slice_max(fit, n= 3),
+          aes(fill = target, colour = target,
+              size = fit),
+          alpha = 0.5)+
+  scale_size("ppb / yr")+
+  scale_y_continuous(limits = st_coordinates(limUS)[,2])+
+  scale_x_continuous(limits = st_coordinates(limUS)[,1]) +
+  # scale_fill_scico(palette = "berlin")
+  # facet_nested(flip_dir+transformation~spc)+
+  #scale_colour_viridis_c(limits = c(2000, 2023))+
+  theme_minimal() +
+  # facet_wrap(~switch)+
+  scale_color_viridis_d() +
+  scale_fill_viridis_d()
+
+test = explore_all_sf |>
+  filter(target == "yes")
 
 ######### Count the number of arrows at different slope magnitudes ##########
 
@@ -129,8 +203,8 @@ mda8_uncertainty_count = mda8_all |>
 
 # Percentage of slopes at each direction within a given region (US or EU), at a given tau, at both the
 # start and end slope #
-# e.g. what percentage of high magnitude positive trends are there in the region/tau/year group?
-# (does not account for uncertainty)
+# e.g. what percentage of positive trends are there in the region/tau/year group?
+# (does not account for uncertainty, or slope magnitude)
 mda8_slope_count = mda8_all |>
   filter(fit != 0) |>
   #filter(year == "first") |>
@@ -139,9 +213,43 @@ mda8_slope_count = mda8_all |>
   select(map_region, year, tau, direction) |>
   group_by(map_region, year, tau, direction) |>
   count() |>
+  ungroup() |>
   group_by(map_region, year, tau) |>
   mutate(percentage = n / sum(n)*100) |>
-  pivot_wider(names_from = tau, values_from = c(n, percentage))
+  filter(tau %in% c(0.05,0.5,0.95)) |>
+  filter(year == 2002) |>
+  #filter(tau %in% 0.95) |>
+  filter(map_region == "EU")
+  #pivot_wider(names_from = tau, values_from = c(n, percentage))
+
+# Percentage of slopes at each magnitude/direction within a given region (US or EU), at a given tau, at both the
+# start and end slope #
+# e.g. what percentage of high magnitude positive trends are there in the region/tau/year group?
+# (does not account for uncertainty)  Percentage is "of the increasing/decreasing trends".
+
+mda8_slope_detailed_count_per_direction = mda8_all |>
+  filter(fit != 0) |>
+  #filter(year == "first") |>
+  mutate(uncertainty = ifelse(uncertainty == "very_high_certainty", "high_certainty", uncertainty),
+         direction = ifelse(fit > 0, "increasing", "decreasing"),
+         slopeBin = case_when(fit > 0 & fit < 0.5 ~ "small_increase",
+                              fit >= 0.5 & fit < 1.5 ~ "moderate_increase",
+                              fit >= 1.5 & fit < 2.5 ~ "large_increase",
+                              fit >= 2.5 ~ "very_large_increase",
+                              fit < 0 & fit > -0.5 ~ "small_decrease",
+                              fit <= -0.5 & fit > -1.5 ~ "moderate_decrease",
+                              fit <= -1.5 & fit > -2.5 ~ "large_decrease",
+                              fit <= -2.5 ~ "very_large_decrease",
+                              .default = NA)) |>
+  select(map_region, year, tau, direction, slopeBin) |>
+  group_by(map_region, year, tau, direction, slopeBin) |>
+  count() |>
+  group_by(map_region, year, tau, direction) |>
+  mutate(percentage = n / sum(n)*100) |>
+  pivot_wider(names_from = tau, values_from = c(n, percentage)) |>
+  filter(map_region == "EU") |>
+  #filter(year == 2019) |>
+  filter(direction == "decreasing")
 
 # Percentage of slopes at each magnitude/direction within a given region (US or EU), at a given tau, at both the
 # start and end slope #
@@ -167,7 +275,9 @@ mda8_slope_detailed_count = mda8_all |>
   count() |>
   group_by(map_region, year, tau) |>
   mutate(percentage = n / sum(n)*100) |>
-  pivot_wider(names_from = tau, values_from = c(n, percentage))
+  pivot_wider(names_from = tau, values_from = c(n, percentage)) |>
+  filter(map_region == "EU") |>
+  filter(year == 2002)
 
 # Percentage of slopes at each magnitude/uncertainty/direction within a given region (US or EU), at a given tau, at both the
 # start and end slope #
@@ -190,7 +300,12 @@ mda8_slope_uncertainty_count = mda8_all |>
   select(map_region, year, tau, direction, uncertainty, slopeBin) |>
   group_by(map_region, year, tau, direction, uncertainty, slopeBin) |>
   count() |>
+  filter(tau %in% c(0.05, 0.5, 0.95)) |>
   group_by(map_region, year, tau) |>
-  mutate(percentage = n / sum(n)*100) |>
-  pivot_wider(names_from = tau, values_from = c(n, percentage))
+  mutate(percentage = n / sum(n, na.rm = T)*100) |>
+  pivot_wider(names_from = tau, values_from = c(n, percentage)) |>
+  filter(direction == "increasing") |>
+  filter(uncertainty %in% c("medium_certainty", "high_certainty")) |>
+  filter(year == 2002) |>
+  filter(map_region == "EU")
 
