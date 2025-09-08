52891a1a3f9c51aa4c52081797886866fc6e7500
willdrysdale <will.drysdale@york.ac.uk>
2025-09-08 17:52:27 +0100

    update arrow maps incl selected ones for paper_figures

---
+++ figure_scripts/arrow_plots_selected.R
@@ -0,0 +1,171 @@
+library(sf)
+library(DBI)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(mirai)
+library(ggtext)
+library(stringr)
+library(ggplot2)
+
+source(here::here('functions','utils.R'))
+source(here::here('functions','plotting_utils.R'))
+
+con = connect_to_db()
+
+tables = c("piecewise_stats_freeTau_mda8_anom_all", "piecewise_stats_freeTau_mda8_anom_cold", "piecewise_stats_freeTau_mda8_anom_warm")
+
+mycrs = 4087 #8857
+
+datRaw = map(
+  tables,
+  ~{
+
+    source(here::here('functions','plotting_utils.R'))
+    temp = .x |>
+      get_slopes() |>
+      dplyr::ungroup() |>
+      filter(tau %in% c(0.05, 0.5, 0.95))
+
+    if("metric" %in% names(temp)){
+      temp$type = temp$metric
+    }else{
+      temp$type = .x
+      temp$fit = temp$fit*365
+    }
+
+    temp
+
+  },
+  .progress = TRUE
+) |>
+  bind_rows()
+
+slopes = datRaw |>
+  make_pvStr(7) |>
+  group_by(type) |>
+  mutate(
+    fit_capped = case_when(
+      fit > 2.5 ~ 2.5,
+      fit < -2.5 ~ -2.5,
+      TRUE ~ fit
+    )) |>
+  ungroup()
+
+line_groups = slopes |>
+  left_join(
+    combinedMetaRegion(con) |>
+      select(station_id, latitude, longitude, region) |>
+      collect() |>
+      distinct(),
+    by = "station_id"
+  ) |>
+  nest_by(type) |>
+  mutate(rm = max(abs(data$fit_capped))) |>
+  mutate(data = data |>
+           calc_arrow_end(rangeMax = rm,
+                          slope = "fit_capped",
+                          length = 3) |>
+           rename(latitude_start = latitude,
+                  longitude_start = longitude) |>
+           mutate(rn = row_number()) |>
+           pivot_longer(c(latitude_start,latitude_end, longitude_start, longitude_end),
+                        names_to = c("coordType","suffix"),
+                        names_sep = "_",
+                        values_to = "coord"
+           ) |>
+           pivot_wider(names_from = "coordType",
+                       values_from = "coord") |>
+           select(-suffix) |>
+           list())
+
+tictoc::tic()
+mirai::daemons(3)
+lines = line_groups |>
+  ungroup() |>
+  mutate(lines = map(data,
+                     in_parallel(
+                       ~{
+                         .x |>
+                           sf::st_as_sf(coords = c("longitude", "latitude"),  crs = 4326) |>
+                           dplyr::group_by(rn) |>
+                           dplyr::summarise(do_union = FALSE) |>
+                           sf::st_cast("LINESTRING")
+                       }
+                     )
+  )
+  )
+mirai::daemons(0)
+tictoc::toc()
+
+
+lineDat = lines |>
+  rowwise() |>
+  mutate(lineDat = data |>
+           left_join(lines, "rn") |>
+           select(-latitude, -longitude, -rn) |>
+           st_as_sf() |>
+           # select(-pvStr) |>
+           make_pvStr(7) |>
+           list()
+  ) |>
+  ungroup() |>
+  rowwise() |>
+  mutate(
+    g_eu_o3 = arrow_plot(
+      dat =lineDat,
+      name = "o3",
+      region = "Europe",
+      type = type,
+      rm = rm,
+      yrs = c(2004, 2018)) |>
+      list(),
+    g_eu_no2 = arrow_plot(
+      dat = lineDat,
+      region ="Europe",
+      name = "no2",
+      type = type,
+      rm = rm,
+      yrs = c(2004, 2018)) |>
+      list(),
+    g_us_o3 = arrow_plot(
+      dat = lineDat,
+      region ="United States of America",
+      name = "o3",
+      type = type,
+      rm = rm,
+      yrs = c(2004, 2018)) |>
+      list(),
+    g_us_no2 = arrow_plot(
+      dat = lineDat,
+      region ="United States of America",
+      name = "no2",
+      type = type,
+      rm = rm,
+      yrs = c(2004, 2018)) |>
+      list()
+  )
+
+dirOut = here::here('figures','paper_figures', 'o3_map')
+
+if(!dir.exists(dirOut)){
+  dir.create(dirOut, recursive = T)
+}
+
+for(i in 1:nrow(lineDat)){
+
+  pdf(
+    here::here(dirOut, paste0("o3_map_",lineDat$type[[i]],"_eu_o3.pdf")),
+    width = 7, height = 10
+  )
+  print(lineDat$g_eu_o3[[i]])
+  dev.off()
+
+  pdf(
+    here::here(dirOut, paste0("o3_map_",lineDat$type[[i]],"_us_o3.pdf")),
+    width = 7, height = 10
+  )
+  print(lineDat$g_us_o3[[i]])
+  dev.off()
+
+}
